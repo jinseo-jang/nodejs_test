c2f1c7cef7db545144ebb02279295fd7331fb359
jinseo-jang <jinseo.jang@gmail.com>
2017-10-17 16:52:54 -0700

    Committed for adding log print in app.js

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ var express = require('express')
 
 app.get('/', function (req, res) {
 res.send('Hello, Docker Test');
+console.log('GET/ Main page reaquested');
 });
 
 app.get('/world.html', function (req, res) {
 res.send('Hello World');
+console.log('GET/ html page reaquested')
 });
 
-server.listen(8080, function() {
+server.listen(8888, function() {
 console.log('Express server listening on port ' + server.address().port);
 });
 // var express = require('express');
